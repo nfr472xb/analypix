a9fb6ea9cbd34334f230b2aa3b13ec0703e7bd49
xerviam <xerviam@gmail.com>
2017-12-25 18:50:00 +0800

    update index posts users related

---
+++ routes/posts.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('post', {title: 'posts'});
+});
+
+router.get('/:postId', function(req, res, next) {
+  res.send('This is ' + req.params.postId + '\'s  page.');
+});
+
+router.get('/:postId/related', function(req, res, next) {
+  res.render('related', {title: 'related'});
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', {title: 'Users'});
 });
 
 router.get('/:userId', function(req, res, next) {
   res.send('This is user ' + req.params.userId + '\'s personal page.');
 });
 
-router.get('/:userId/:postId', function(req, res , next){
-  res.send('This is user <b>' + req.params.userId + '</b>\'s post page, postId is <b>' + req.params.postId + '</b>');
-});
 
 module.exports = router;
