f468891940de53bb1734a16b9d943ee24a4abc2c
xerviam <xerviam@gmail.com>
2017-12-25 20:02:29 +0800

    update nav footer template

--- README.md
+++ README.md
@@ -5,10 +5,13 @@
 路由設定
 
 / ->全體動態
-
+/login
+/register
 /users/userId -> 使用者動態
 /posts/postId -> 貼文詳細資訊
 /posts/postId/related  -> 相關貼文
+模板
+nav.ejs
 
 伺服器測試 -> $nodemon ./bin/www
 
--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-  res.render('users', {title: 'Users'});
-});
-
 router.get('/:userId', function(req, res, next) {
   res.send('This is user ' + req.params.userId + '\'s personal page.');
 });
-
 
 module.exports = router;
